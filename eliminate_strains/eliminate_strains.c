3923f6afa3c48710407d6dd80cddc50a48cbf857
vicpagan <vicpagan808@gmail.com>
2026-08-18 02:45:39 -1000

    Retesting again

--- eliminate_strains/eliminate_strains.c
+++ eliminate_strains/eliminate_strains.c
@@ -103,15 +103,15 @@ int main(int argc, char **argv)
 
 		sam_filepaths[ref_idx] = get_filepath_in_working_dir(sam_filename, opt.working_dir);
 
-		perform_bowtie_alignment_xeq(bowtie2_reference_filepaths[ref_idx], opt.single_end_filepath, opt.forward_end_filepath, opt.reverse_end_filepath, sam_filepaths[ref_idx], opt.working_dir, opt.paired, opt.fasta_format, opt.verbose);
-		int invoke_cleaning = calculate_error_rates(sam_filepaths[ref_idx], opt.end_region_length, opt.end_region_error_mult);
-		if (invoke_cleaning == 1 && opt.clean_reads == 0)
-		{
-			// FIXME: As of now, this changes where the opt.xxx_filepath points to once clean_reads() is called. 
-			// Since we now have multiple reference strains, we should determine how to handle this (either all are cleaned, or only the ones that need cleaning are cleaned)
-			printf("Error rates of reads are too high! Cleaning reads...\n");
-			clean_reads(opt.single_end_filepath, opt.forward_end_filepath, opt.reverse_end_filepath, opt.working_dir, opt.paired, opt.fasta_format, opt.sequence_length_threshold, opt.trim_length, opt.fastq_trimmer_threshold);
-		}
+		// perform_bowtie_alignment_xeq(bowtie2_reference_filepaths[ref_idx], opt.single_end_filepath, opt.forward_end_filepath, opt.reverse_end_filepath, sam_filepaths[ref_idx], opt.working_dir, opt.paired, opt.fasta_format, opt.verbose);
+		// int invoke_cleaning = calculate_error_rates(sam_filepaths[ref_idx], opt.end_region_length, opt.end_region_error_mult);
+		// if (invoke_cleaning == 1 && opt.clean_reads == 0)
+		// {
+		// 	// FIXME: As of now, this changes where the opt.xxx_filepath points to once clean_reads() is called. 
+		// 	// Since we now have multiple reference strains, we should determine how to handle this (either all are cleaned, or only the ones that need cleaning are cleaned)
+		// 	printf("Error rates of reads are too high! Cleaning reads...\n");
+		// 	clean_reads(opt.single_end_filepath, opt.forward_end_filepath, opt.reverse_end_filepath, opt.working_dir, opt.paired, opt.fasta_format, opt.sequence_length_threshold, opt.trim_length, opt.fastq_trimmer_threshold);
+		// }
 		perform_bowtie_alignment(bowtie2_reference_filepaths[ref_idx], opt.single_end_filepath, opt.forward_end_filepath, opt.reverse_end_filepath, sam_filepaths[ref_idx], opt.working_dir, opt.paired, opt.fasta_format, opt.verbose);
 
 		references_data_str.reference_names[ref_idx] = read_fastx_header_name(msa_reference_filepaths[ref_idx]);
