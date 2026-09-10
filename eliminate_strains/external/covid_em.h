40433b57ca18c304a265682d746ba3dc279dee50
vicpagan <vicpagan808@gmail.com>
2026-09-10 13:52:21 -1000

    fix

--- eliminate_strains/external/covid_em.h
+++ eliminate_strains/external/covid_em.h
@@ -1,10 +1,10 @@
 #ifndef COVID_EM_H
 #define COVID_EM_H
 
-void em_update(const double *proportions, const double **q_matrix, int num_reads, int num_strains, double *updated_proportions);
+void em_update(const double *proportions, const double **q_matrix, int num_reads, int num_strains, double *updated_proportion, int num_threads);
 
-double log_likelihood(const double *proportions, const double **q_matrix, int num_reads, int num_strains);
+double log_likelihood(const double *proportions, const double **q_matrix, int num_reads, int num_strains, int num_threads);
 
-double negative_log_likelihood(const double *proportions, const double **q_matrix, int num_reads, int num_strains);
+double negative_log_likelihood(const double *proportions, const double **q_matrix, int num_reads, int num_strains, int num_threads);
 
 #endif
